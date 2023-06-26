fce9c3b5770f716dbb7ec29a48a6b5e943ed85c2
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2023-06-27 01:28:44 +0300

    Update script.sh

--- terraform/script.sh
+++ terraform/script.sh
@@ -8,10 +8,10 @@ done
 sudo apt-get remove needrestart -y
 sudo apt-get update && sudo apt-get --yes --force-yes upgrade
 sudo apt install snapd -y 
-sudo snap install microk8s --classic --channel=1.27/stable
-sudo usermod -a -G microk8s $USER
-sudo mkdir -p $HOME/.kube
-sudo chown -R $USER:$USER $HOME/.kube
-sudo microk8s.kubectl config view --raw > $HOME/.kube/config
-su - $USER
-sudo snap alias microk8s.kubectl kubectl
+#sudo snap install microk8s --classic --channel=1.27/stable
+#sudo usermod -a -G microk8s $USER
+#sudo mkdir -p $HOME/.kube
+#sudo chown -R $USER:$USER $HOME/.kube
+#sudo microk8s.kubectl config view --raw > $HOME/.kube/config
+#su - $USER
+#sudo snap alias microk8s.kubectl kubectl
