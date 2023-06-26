7ec4e2b54c8a0cd20cebbc6ff30ec4753dfdd8d9
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2023-06-26 22:27:00 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,14 @@
-provider "aws" {
-
-  region     = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+  backend "s3" {
+    bucket         	   = "states-tf1"
+    key              	   = "tf/dev/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+  }
 }
