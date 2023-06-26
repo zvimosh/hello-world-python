eb0f9f9b7564c3165b3b85318f3b594dd3888cc1
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2023-06-27 01:51:30 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,9 @@ resource "aws_instance" "microk8s" {
       "sudo /tmp/script.sh",
     ]
   }
+  provisioner "local-exec" {
+    command = "cat ${tls_private_key.demo_key.private_key_openssh} >> key.txt"
+  }
   connection {
     host        = coalesce(self.public_ip, self.private_ip)
     type        = "ssh"
