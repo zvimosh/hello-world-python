15dc64c5b7a9a8c1e72e0346fa631877482be8c4
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2023-06-26 23:41:13 +0300

    Update script.sh

--- terraform/script.sh
+++ terraform/script.sh
@@ -4,3 +4,14 @@
 until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
   sleep 1
 done
+
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install snapd -y
+sudo snap install microk8s --classic --channel=1.27/stable
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+su - $USER
+alias kubectl='microk8s kubectl'
+
