76c64c7efd5a298bc065736c71da4fb5c3d23048
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2023-06-26 21:05:37 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+
+
+resource "aws_instance" "microk8s" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.mykey.key_name
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters.
+      "sudo /tmp/script.sh",
+    ]
+  }
+  connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
+}
