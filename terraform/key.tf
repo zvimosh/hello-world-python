91e4dc9d8864c6158ecd2b43f28cce781debbc5c
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2023-06-26 22:38:41 +0300

    Update key.tf

--- terraform/key.tf
+++ terraform/key.tf
@@ -1,4 +1,11 @@
+resource "tls_private_key" "demo_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+
 resource "aws_key_pair" "mykeypair" {
   key_name   = "mykeypair"
-  public_key = file("./key.pub")
+  public_key = tls_private_key.demo_key.public_key_openssh
 }
