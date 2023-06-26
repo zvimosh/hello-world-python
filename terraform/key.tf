d189f738b83c8ae39d77ec4b444879658505b8d5
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2023-06-27 02:35:53 +0300

    Update key.tf

--- terraform/key.tf
+++ terraform/key.tf
@@ -8,4 +8,11 @@ resource "tls_private_key" "demo_key" {
 resource "aws_key_pair" "mykeypair" {
   key_name   = "mykeypair"
   public_key = tls_private_key.demo_key.public_key_openssh
+# Store private key :  Generate and save private key(aws_keys_pairs.pem) in current directory
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo '${tls_private_key.terrafrom_generated_private_key.private_key_pem}' > aws_keys_pairs.pem
+      chmod 400 aws_keys_pairs.pem
+    EOT
+  }
 }
