ff7a71cc2cfc5a0e90c3796473a9ca1dc15fbde9
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2023-06-27 03:09:17 +0300

    Update script.sh

--- terraform/script.sh
+++ terraform/script.sh
@@ -6,12 +6,18 @@ until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
 done
 
 sudo apt-get remove needrestart -y
-#sudo apt-get update && sudo apt-get --yes --force-yes upgrade
-#sudo apt install snapd -y 
-#sudo snap install microk8s --classic --channel=1.27/stable
-#sudo usermod -a -G microk8s $USER
-#sudo mkdir -p $HOME/.kube
-#sudo chown -R $USER:$USER $HOME/.kube
-#sudo microk8s.kubectl config view --raw > $HOME/.kube/config
-#su - $USER
-#sudo snap alias microk8s.kubectl kubectl
+sudo apt-get update && sudo apt-get --yes --force-yes upgrade
+sudo apt install snapd -y
+sudo snap install microk8s --classic --channel=1.27/stable
+sudo usermod -a -G microk8s $USER
+sudo mkdir -p $HOME/.kube
+sudo chown -R $USER:$USER $HOME/.kube
+sudo microk8s.kubectl config view --raw > $HOME/.kube/config
+newgrp microk8s
+sudo snap alias microk8s.kubectl kubectl
+microk8s enable ingress prometheus dashboard hostpath-storage metrics-server observability
+kubectl patch svc kube-prom-stack-grafana -n observability --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'
+kubectl patch svc kubernetes-dashboard -n kube-system --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'
+kubectl patch svc kube-prom-stack-grafana -n observability --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":30040}]'
+kubectl patch svc kubernetes-dashboard -n kube-system --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"},{"op":"replace","path":"/spec/ports/0/nodePort","value":30041}]'
+kubectl apply -f /tmp/deploy.yml
